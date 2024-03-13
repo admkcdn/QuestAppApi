1ca876bde45896c822b4c0365325418afb745e56
Adem Kocdogan <adem.kocdogan@icloud.com>
2024-03-13 13:09:55 +0300

    Data property changed.

--- src/main/java/com/project/QuestApp/entities/User.java
+++ src/main/java/com/project/QuestApp/entities/User.java
@@ -1,9 +1,7 @@
 package com.project.QuestApp.entities;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
@@ -11,6 +9,7 @@ import lombok.Data;
 @Data
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     String username;
     String password;
